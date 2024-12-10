5ae1689768b7390fd0e4f43356eac5e13670686e
Oskar Nesheim <nesheimo@gmail.com>
2024-12-10 14:03:51 +0100

    ran tests for both naked and finetuned BERT model
    Finetuned BERT obliterated the naked BERT model - .493 to .099 accuracy

--- BERT_finetuned.py
+++ BERT_finetuned.py
@@ -13,29 +13,36 @@ import re
 from sklearn.metrics import accuracy_score
 
 # Load data
-df = pd.read_json('movies.json')
+df = pd.read_json('movies_balanced.json')
 
 # Preprocessing text
+
+
 def preprocess_text(s):
     s = re.sub(r'[^\w\s]', '', s)  # Remove punctuation
     s = s.lower()  # Lowercase text
     return s
 
+
 df['description'] = df['description'].apply(preprocess_text)
 
 # Filter the dataframe for only relevant genres for each iteration. We have ran the code with 2, 3, 4, 5 and all 19 genres.
-df = df[df['genre'].isin(['Action', 'Drama', 'Comedy', 'Horror', 'Animation', 'Adventure', 'Thriller', 'Romance', 'Crime', 'Science Fiction', 'Family', 'Fantasy', 'Mystery', 'Documentary', 'Western', 'War', 'Music', 'History', 'TV Movie'])]
+df = df[df['genre'].isin(['Action', 'Drama', 'Comedy', 'Horror', 'Animation', 'Adventure', 'Thriller', 'Romance', 'Crime',
+                         'Science Fiction', 'Family', 'Fantasy', 'Mystery', 'Documentary', 'Western', 'War', 'Music', 'History', 'TV Movie'])]
 
 # Initialize BERT tokenizer
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
+
 def tokenize_function(texts):
     return tokenizer(texts, add_special_tokens=True, max_length=120, padding='max_length', truncation=True, return_tensors='pt')
+
 
 # Tokenize all descriptions
 encoded_batch = tokenize_function(df['description'].tolist())
 df['input_ids'] = [tensor.squeeze() for tensor in encoded_batch['input_ids']]
-df['attention_mask'] = [tensor.squeeze() for tensor in encoded_batch['attention_mask']]
+df['attention_mask'] = [tensor.squeeze()
+                        for tensor in encoded_batch['attention_mask']]
 
 # Encode labels
 label_encoder = LabelEncoder()
@@ -46,6 +53,8 @@ train_df, temp_df = train_test_split(df, test_size=0.2, random_state=42)
 val_df, test_df = train_test_split(temp_df, test_size=0.5, random_state=42)
 
 # Create PyTorch dataset
+
+
 class MovieDataset(Dataset):
     def __init__(self, entries):
         self.entries = entries
@@ -60,6 +69,7 @@ class MovieDataset(Dataset):
         labels = torch.tensor(entry['labels'], dtype=torch.long)
         return {'input_ids': input_ids, 'attention_mask': attention_mask, 'labels': labels}
 
+
 train_dataset = MovieDataset(train_df)
 val_dataset = MovieDataset(val_df)
 test_dataset = MovieDataset(test_df)
@@ -70,11 +80,13 @@ val_loader = DataLoader(val_dataset, batch_size=batch_size)
 test_loader = DataLoader(test_dataset, batch_size=batch_size)
 
 # Load BERT model for sequence classification
-model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=len(label_encoder.classes_))
+model = BertForSequenceClassification.from_pretrained(
+    'bert-base-uncased', num_labels=len(label_encoder.classes_))
 model.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
 
 optimizer = AdamW(model.parameters(), lr=5e-5)
-scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=len(train_loader) * 3)
+scheduler = get_linear_schedule_with_warmup(
+    optimizer, num_warmup_steps=0, num_training_steps=len(train_loader) * 3)
 
 best_model = None
 best_val_loss = float('inf')
@@ -124,11 +136,14 @@ accuracy = accuracy_score(actuals, predictions)
 print(f'Accuracy: {accuracy:.4f}')  # Print the accuracy with 4 decimal places
 
 # Generate classification report and modify index to show genre names
-report = pd.DataFrame(classification_report(actuals, predictions, output_dict=True)).transpose()
-report.drop(['accuracy'], inplace=True)  # Remove the 'accuracy' row if it's present
+report = pd.DataFrame(classification_report(
+    actuals, predictions, output_dict=True)).transpose()
+# Remove the 'accuracy' row if it's present
+report.drop(['accuracy'], inplace=True)
 report['support'] = report['support'].apply(int)
 # Map numeric labels back to string names using LabelEncoder
-report.index = [label_encoder.inverse_transform([int(idx)])[0] if idx.isdigit() else idx for idx in report.index]
+report.index = [label_encoder.inverse_transform(
+    [int(idx)])[0] if idx.isdigit() else idx for idx in report.index]
 
 # Add a print for the classification report
 print(report)
@@ -143,7 +158,8 @@ plt.show()
 
 # Confusion Matrix with genre names
 conf_mat = confusion_matrix(actuals, predictions)
-sns.heatmap(conf_mat, annot=True, fmt='d', xticklabels=label_encoder.classes_, yticklabels=label_encoder.classes_, cmap='Blues')
+sns.heatmap(conf_mat, annot=True, fmt='d', xticklabels=label_encoder.classes_,
+            yticklabels=label_encoder.classes_, cmap='Blues')
 plt.xlabel('Predicted')
 plt.ylabel('True')
 plt.title('Confusion Matrix')
